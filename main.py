a44ba403ecd2c27429f79dbaeaf508f7aa50929a
ERHAN ŞEN <77855407+erhansennx@users.noreply.github.com>
2024-02-01 18:17:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:black'>Hello World! This is a Flask web app.</h1>"
+    return "<h1 style='color:black'>Hello World! This is a Flask web app..</h1>"
 
 
 if __name__ == "__main__":
